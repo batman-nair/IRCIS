07bb7e4255af48cb1e8f63184bf39084bad30491
bhudgeons <brandon@atsoft.at>
2020-05-19 14:31:10 -0500

    Adding ^ and binary arithmetic operators

--- src/CharMaps.cc
+++ src/CharMaps.cc
@@ -2,7 +2,7 @@
 
 namespace Ircis {
 
-  DEF_CHAR_GROUP(arith, CH_ADD, CH_SUB, CH_DIV, CH_MUL, CH_MOD)
+  DEF_CHAR_GROUP(arith, CH_ADD, CH_SUB, CH_DIV, CH_MUL, CH_MOD, CH_POW, CH_AND, CH_OR, CH_XOR, CH_BL, CH_BR)
   DEF_CHAR_GROUP(blank, CH_SPC, CH_DOT)
 
 }
